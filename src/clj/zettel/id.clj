f09ed739ca49c9816cdfa20424911599bcc24e71
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-06-14 20:29:42 -0300

    Implement zettel.id

---
+++ src/clj/zettel/id.clj
@@ -0,0 +1,55 @@
+(ns zettel.id
+  (:require [clojure.spec.alpha :as s]
+            [clojure.string :as str]
+            [failjure.core :as f]
+            [failjure.spec :as failjure.spec]))
+
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Namespace constants                                                    ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
+(def ^:private ^:const n 4)
+
+(def ^:private ^:const words
+  (->> "./etc/bip39.txt" slurp str/split-lines (map keyword)))
+
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Type specs                                                             ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
+(s/def ::word (set words))
+
+(s/def ::zettel.id (s/coll-of ::word :into [] :count n))
+
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Public - Constructors                                                  ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
+(s/fdef random
+  :args (s/cat)
+  :ret ::zettel.id)
+(defn random
+  []
+  (vec (repeatedly n #(rand-nth words))))
+
+(s/fdef str->
+  :args (s/cat :s string?)
+  :ret  (s/or :ok ::zettel.id :fail ::failjure.spec/failure))
+(defn str->
+  [s]
+  (let [substrings (str/split s #"-")
+        hopefuly-an-id (map keyword substrings)]
+    (if (s/valid? ::zettel.id hopefuly-an-id)
+      (vec hopefuly-an-id)
+      (f/fail (str "Failed to parse id string: " s)))))
+
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Public - Serialization                                                 ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
+(s/fdef ->str
+  :args (s/cat :id ::zettel.id)
+  :ret  string?)
+(defn ->str
+  [id]
+  (->> id (map name) (str/join "-")))
