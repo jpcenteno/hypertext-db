1be40c603e35647d2d01908c957e0ecab34e2ac1
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-06-14 22:37:38 -0300

    Refactor move tmp fixtures to their own directory

---
+++ test/clj/zettel/helpers/tmp.clj
@@ -0,0 +1,27 @@
+(ns zettel.helpers.tmp
+  (:require [clojure.java.io :as io]))
+
+(def ^:dynamic dir :dir-not-initialized)
+
+(defn- rm
+  [file]
+  (run! io/delete-file (reverse (file-seq file))))
+
+(defn- random-tmp-dir-file []
+  (io/file (System/getProperty "java.io.tmpdir")
+           (str "zettel-test-" (int (rand Integer/MAX_VALUE)))))
+
+(defn- create-tmp-dir []
+  (loop [attempts-left 10]
+    (if (zero? attempts-left)
+      (throw (Exception. "Failed to create tmp dir for the vault"))
+      (let [dir (random-tmp-dir-file)]
+        (if (.mkdir dir)
+          dir
+          (recur (dec attempts-left)))))))
+
+(defmacro with-tmp-dir
+  [body]
+  `(binding [dir (#'create-tmp-dir)]
+     (try (~@body)
+          (finally (#'rm dir)))))
--- test/clj/zettel/vault_test.clj
+++ test/clj/zettel/vault_test.clj
@@ -1,48 +1,23 @@
 (ns zettel.vault-test
   (:require [clojure.test :refer [deftest is testing]]
+            [zettel.helpers.tmp :as tmp]
             [zettel.vault :as vault]
-            [clojure.spec.alpha :as s]
-            [clojure.java.io :as io]))
-
-(def ^:dynamic *dir* :dir-not-initialized)
-
-(defn- rm
-  [file]
-  (run! io/delete-file (reverse (file-seq file))))
-
-(defn- random-tmp-dir-file []
-  (io/file (System/getProperty "java.io.tmpdir")
-           (str "zettel-test-" (int (rand Integer/MAX_VALUE)))))
-
-(defn- create-tmp-dir []
-  (loop [attempts-left 10]
-    (if (zero? attempts-left)
-      (throw (Exception. "Failed to create tmp dir for the vault"))
-      (let [dir (random-tmp-dir-file)]
-        (if (.mkdir dir)
-          dir
-          (recur (dec attempts-left)))))))
-
-(defmacro with-tmp-dir
-  [body]
-  `(binding [*dir* (create-tmp-dir)]
-     (try (~@body)
-          (finally (rm *dir*)))))
+            [clojure.spec.alpha :as s]))
 
 (deftest dir->
   (testing "Creates a ::zettel.vault instance"
-    (with-tmp-dir
-      (is (s/valid? ::vault/t (vault/dir-> *dir*))))))
+    (tmp/with-tmp-dir
+      (is (s/valid? ::vault/t (vault/dir-> tmp/dir))))))
 
 (comment
   (deftest get-file-list
     (testing "Empty dir returns empty collection"
-      (with-tmp-dir
-        (let [vault (vault/dir-> *dir*)
+      (tmp/with-tmp-dir
+        (let [vault (vault/dir-> tmp/dir)
               result (vault/get-file-list vault)]
           (is (empty? result)))))
     (testing "Lists files in vault where filename matches an ID"
-      (with-tmp-dir
-        (let [vault (vault/dir-> *dir*)
+      (tmp/with-tmp-dir
+        (let [vault (vault/dir-> tmp/dir)
               result (vault/get-file-list vault)]
           (is (empty? result)))))))
