352d34408851493df741d1506f477742493eafd7
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-06-15 12:38:21 -0300

    Implement id-string?, bug fixes

--- src/clj/zettel/id.clj
+++ src/clj/zettel/id.clj
@@ -38,10 +38,18 @@
 (defn str->
   [s]
   (let [substrings (str/split s #"-")
-        hopefuly-an-id (map keyword substrings)]
+        hopefuly-an-id (vec (map keyword substrings))]
     (if (s/valid? ::zettel.id hopefuly-an-id)
-      (vec hopefuly-an-id)
+      hopefuly-an-id
       (f/fail (str "Failed to parse id string: " s)))))
+
+(s/fdef id-string?
+  :args (s/cat :x any?)
+  :ret boolean?)
+(defn id-string?
+  [x]
+  (and (string? x)
+       (f/ok? (str-> x))))
 
 ; ╔════════════════════════════════════════════════════════════════════════╗
 ; ║ Public - Serialization                                                 ║
@@ -49,7 +57,7 @@
 
 (s/fdef ->str
   :args (s/cat :id ::zettel.id)
-  :ret  string?
+  :ret  id-string?
   :fn   (s/and #(f/ok? (:ret %))
                #(let [input  (-> % :args :id)
                       result (-> % :ret)
