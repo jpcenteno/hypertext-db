5c0c040f52fa500e07f9b107e816c00a16d978fb
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-06-21 16:12:52 -0300

    Declare graph data structure

---
+++ src/clj/zettel/graph.clj
@@ -0,0 +1,99 @@
+(ns zettel.graph
+  (:require [clojure.set             :as set]
+            [clojure.spec.alpha      :as s]
+            [zettel.graph.node       :as node]
+            [zettel.id               :as id]
+            [zettel.vault            :as vault]
+            [zettel.vault.vault-file :as vault-file]))
+
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Type specs : Graph                                                     ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
+(defn- key-equals-node-id? [[k node]]
+  (= k (::vault-file/id node)))
+
+(defn- set-does-not-contain-key? [[k ids]]
+  (not (contains? ids k)))
+
+(s/def ::nodes (s/and (s/map-of ::id/t ::node/t)
+                      (s/every key-equals-node-id?)))
+
+;; Backlinks exists to keep track of links to nodes that are not in the graph
+;; yet or do not exist.
+(s/def ::backlinks (s/and (s/map-of ::id/t ::node/backlinks)))
+
+(s/fdef has-backlink?
+  :args (s/cat :backlinks ::backlinks :from ::id/t :to ::id/t)
+  :ret  boolean?)
+(defn- has-backlink? [backlinks from to]
+  (contains? (get backlinks to #{}) from))
+
+(s/fdef every-link-in-backlinks?
+  :args (s/cat :k (s/keys :req [::nodes ::backlinks]))
+  :ret  boolean?)
+(defn- every-link-in-backlinks? [{::keys [nodes backlinks]}]
+  (let [links (->> nodes
+                   (#(map    (fn [[from node]] [from (::node/links node)]) %))
+                   (#(mapcat (fn [[from links]] (map (fn [to] [from to]) links)) %)))]
+    (every? (fn [[from to]] (has-backlink? backlinks from to))
+            links)))
+
+(s/fdef every-backlink-comes-from-a-node?
+  :args (s/cat :k (s/keys :req [::nodes ::backlinks]))
+  :ret  boolean?)
+(defn- every-backlink-comes-from-a-node?
+  [{::keys [nodes backlinks]}]
+  (let [from-coll (->> backlinks vals (reduce set/union))]
+    (every? #(contains? nodes %) from-coll)))
+
+(defn- flattened-backlinks
+  [backlinks]
+  (mapcat (fn [[to froms]] (map (fn [from] {:to to :from from})
+                                froms))
+          backlinks))
+(defn- has-link?
+  [nodes {:keys [from to]}]
+  (contains? (-> nodes (get from) ::node/links) to))
+(defn- every-backlink-in-links?
+  [{::keys [nodes backlinks]}]
+  (let [bs (flattened-backlinks backlinks)]
+    (every? #(has-link? nodes %) bs)))
+
+(comment
+  (->
+   {::nodes {1 {::node/links #{}}
+             2 {::node/links #{1}}
+             3 {::node/links #{1 2}}}
+    ::backlinks {1 #{2 3}
+                 2 #{3}}}
+   every-backlink-in-links?))
+
+(s/def ::t (s/and ::vault/t
+                  (s/keys :req [::nodes ::backlinks])
+                  every-link-in-backlinks?
+                  every-backlink-comes-from-a-node?
+                  every-backlink-in-links?))
+
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Constructor                                                            ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
+(s/fdef vault->
+  :args (s/cat :vault ::vault/t)
+  :ret  ::t
+  :fn   (s/and #(set/subset? (-> % :args :vault set) (-> % :ret set))
+               #(-> % :ret ::nodes empty?)))
+(defn vault->
+  "Casts a `::vault/t` into an empty map"
+  [vault]
+  (-> vault
+      (assoc ::nodes {})
+      (assoc ::backlinks {})))
+
+(s/fdef insert-node
+  :args (s/cat :graph ::t :node ::node)
+  :ret  ::t)
+(defn insert-node
+  [graph node]
+  (update graph ::nodes assoc node))
---
+++ test/clj/zettel/graph_test.clj
@@ -0,0 +1,142 @@
+(ns zettel.graph-test
+  (:require [clojure.set             :as set]
+            [clojure.spec.alpha      :as s]
+            [clojure.test            :refer [deftest is testing]]
+            [zettel.graph            :as graph]
+            [zettel.graph.node       :as node]
+            [zettel.helpers.tmp      :as tmp]
+            [zettel.id               :as id]
+            [zettel.vault            :as vault]
+            [zettel.vault.vault-file :as vault-file]))
+
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Test data                                                              ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
+(s/fdef create-node
+  :args (s/cat)
+  :ret ::node/t)
+(defn- create-node []
+  (-> "some.ext" vault-file/random node/vault-file->))
+
+(def ^:private node-bob   (create-node))
+(def ^:private id-bob     (::vault-file/id node-bob))
+(def ^:private node-alice (assoc (create-node) ::node/links #{id-bob}))
+(def ^:private id-alice   (::vault-file/id node-alice))
+
+(comment
+
+  (tmp/with-tmp-dir
+    (let [graph (-> tmp/dir
+                    vault/dir->
+                    graph/vault->
+                    (assoc ::graph/nodes     {id-alice node-alice id-bob node-bob})
+                    (assoc ::graph/backlinks {id-bob #{id-alice}}))]
+      (mapcat (fn [[id node]] (map)) (::graph/nodes graph)))))
+
+((fn [[id node]] (map (partial conj [id]) (::node/links node))) [:the-id {::node/links #{:link-1 :link-2}}])
+
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Type spec tests and documentation                                      ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
+(deftest spec
+  (tmp/with-tmp-dir
+    (let [vault (vault/dir-> tmp/dir)
+          graph (graph/vault-> vault)]
+
+      (testing "Every key in the `::graph/nodes` map must match the node's key:"
+        (testing "Invalid graph"
+          (let [graph (assoc graph ::graph/nodes {id-alice node-bob})]
+            (is (not (s/valid? ::graph/t graph)))))
+        (testing "Valid graph"
+          (let [graph (assoc graph ::graph/nodes {id-bob node-bob})]
+            (is (s/valid? ::graph/t graph)))))
+
+      (testing "Every link from `::graph/nodes` have a corresponding mapping in `::graph/backlinks`:"
+        (testing "Invalid graph"
+          (let [graph (-> graph
+                          (assoc ::graph/nodes {id-alice node-alice}))]
+            (is (not (s/valid? ::graph/t graph)))))
+        (testing "Valid graph"
+          (let [graph (-> graph
+                          (assoc ::graph/nodes {id-alice node-alice})
+                          (assoc ::graph/backlinks {id-bob #{id-alice}}))]
+            (is (s/valid? ::graph/t graph)))))
+
+      (testing "Every backlink id from `::graph/backlinks` must be an existing key in `::graph/nodes`:"
+        (testing "Invalid graph"
+          (let [graph (merge graph
+                             {::graph/nodes     {}
+                              ::graph/backlinks {id-bob #{id-alice}}})]
+            (is (not (s/valid? ::graph/t graph)))))
+
+        (testing "Valid graph"
+          (let [graph (merge graph
+                             {::graph/nodes     {id-alice node-alice}
+                              ::graph/backlinks {id-bob #{id-alice}}})]
+            (is (s/valid? ::graph/t graph)))))
+
+      (testing "Every backlink from `::graph/backlinks` must have a corresponding mapping in `::graph/notes`"
+        (let [node-alice-without-links (assoc node-alice ::node/links #{})
+              graph                    (-> graph
+                                           (assoc ::graph/nodes {id-alice node-alice-without-links})
+                                           (assoc ::graph/backlinks {id-bob #{id-alice}}))]
+
+          (testing "A graph is invalid when a backlink is not reflected by ::graph/nodes"
+            (is (not (s/valid? ::graph/t graph))))
+
+          (testing "We can fix the graph by adding the link to the corresponding node"
+            (is (s/valid? ::graph/t (assoc graph ::graph/nodes {id-alice node-alice}))))))
+
+      (testing "A graph may contain backlinks to unknown nodes"
+        ;; By design, nodes contain link ids without any knowledge about which
+        ;; other nodes belong to the graph (The node domain is a layer below the
+        ;; graph domain).
+        ;;
+        ;; A node might link to a node that wasn't added yet to the graph
+        ;; or a broken link in the worst case scenario.
+        ;;
+        ;; Every operation that adds a node to the graph must replicate every
+        ;; backlink in hopes that the linked node will be added in the future.
+        (let [graph (-> graph
+                        (assoc ::graph/nodes {id-alice node-alice})
+                        (assoc ::graph/backlinks {id-bob #{id-alice}}))]
+          (is (s/valid? ::graph/t graph)))))))
+
+(deftest vault->
+  (tmp/with-tmp-dir
+    (testing "Returns a graph"
+      (let [some-vault (vault/dir-> tmp/dir)
+            graph      (graph/vault-> some-vault)]
+        (is (s/valid? ::graph/t graph))
+
+        (testing "that retails every attribute from the input vault"
+          (is (set/subset? (set some-vault)
+                           (set graph))))
+
+        (testing "without any notes"
+          (is (empty? (::notes graph))))
+
+        (testing "without any backlinks"
+          (is (empty? (::backlinks graph))))))))
+
+(comment
+  (deftest graph-type-spec
+    (testing "Each key in ::nodes matches the node's id"
+      (tmp/with-tmp-dir
+        (let [node              (create-node)
+              mismatching-id    (id/random)
+              graph             (-> tmp/dir
+                                    vault/dir->
+                                    graph/vault->)]
+          (is (s/valid? ::graph/t graph))
+          (is (not (s/valid? ::graph/t (assoc-in graph  [::nodes mismatching-id] node)))))))
+
+    (testing "Each node backlink should point to an existing node"
+    ;; Otherwise, where did the backlink came from?
+      (tmp/with-tmp-dir
+        (let [bad-id (id/random)
+              node   (update (create-node) ::backlinks conj bad-id)
+              graph  (-> tmp/dir vault/dir-> graph/vault-> (graph/insert-node node))]
+          (is (not (s/valid? ::graph/t graph))))))))
