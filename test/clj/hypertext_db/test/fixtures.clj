b47ee1b8488e65ef864168b837a81a3eb2ff783b
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-06-29 13:40:37 -0300

    Fixtures: Implement a generator for ::vault/dir

--- test/clj/hypertext_db/test/fixtures.clj
+++ test/clj/hypertext_db/test/fixtures.clj
@@ -2,9 +2,37 @@
   (:require [clojure.spec.alpha      :as s]
             [clojure.spec.gen.alpha  :as gen]
             [pathetic.core           :as path]
+            [hypertext-db.graph            :as graph]
             [hypertext-db.graph.node       :as node]
+            [hypertext-db.vault            :as vault]
             [hypertext-db.vault.vault-file :as vault-file])
   (:import (java.io File)))
+
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Private helpers                                                        ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
+(defn- create-a-temporary-directory
+  "Creates a temporary directory that will be deleted when the java VM running
+  the tests terminates.
+
+  This is necessary for specs that require existing directories."
+  []
+  (let [prefix "hypertext-db-test-"
+        attrs  (into-array java.nio.file.attribute.FileAttribute [])
+        path   (java.nio.file.Files/createTempDirectory prefix attrs)
+        file   (.toFile path)]
+    (doto file
+      .deleteOnExit)))
+
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Spec re-definitions with added generators                              ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
+(s/def ::vault/dir
+  (s/with-gen ::vault/dir
+    #(gen/fmap (fn [_] (create-a-temporary-directory))
+               (gen/return 0))))
 
 (def ^:private relative-file-generator
   (->> (s/gen (s/+ (s/and string? #(not= "" %)))) ; -> Path stems
