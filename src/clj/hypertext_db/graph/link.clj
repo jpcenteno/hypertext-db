e8559082b299735eda8c87b6d88a7be8810dd7e4
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-07-10 20:16:53 -0300

    Remove unused namespace

--- src/clj/hypertext_db/graph/link.clj
+++
@@ -1,6 +0,0 @@
-(ns hypertext-db.graph.link
-  (:require [clojure.spec.alpha :as s]
-            [hypertext-db.vault.vault-file :as vault-file]))
-
-(s/def ::t (s/and (s/cat :from ::vault-file/id :to ::vault-file/id)
-                  #(not= (first %) (second %))))
