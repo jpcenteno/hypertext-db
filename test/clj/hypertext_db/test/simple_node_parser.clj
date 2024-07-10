3f3f4cae3ea96240880ab44d6c3175ef14b9efb0
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-07-09 22:44:42 -0300

    Cleanup the simple test parser

--- test/clj/hypertext_db/test/simple_node_parser.clj
+++ test/clj/hypertext_db/test/simple_node_parser.clj
@@ -8,29 +8,35 @@
             [hypertext-db.vault.vault-file :as vault-file])
   (:import (java.io File)))
 
-(def extension ".link-list")
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Implementation internals                                               ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
+(def extension ^:private ".link-list")
 
 (s/fdef can-parse?
   :args (s/cat :vault-file ::vault-file/t)
   :ret  boolean?)
-(defn can-parse?
+(defn- can-parse?
   [vault-file]
   (-> vault-file ::vault-file/id (str/ends-with? extension)))
 
 (s/fdef ->node
   :args (s/and (s/cat :vault-file ::vault-file/t :vault ::vault/t))
   :ret  ::node/t)
-(defn ->node
+(defn- ->node
   [vault-file vault]
   (let [content (vault/slurp-vault-file vault vault-file)
         links   (->> content str/split-lines (map #(File. %)) set)]
     (-> vault-file node/vault-file-> (assoc ::node/links links))))
 
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Public test helpers                                                    ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
 (def parser
   {::parser/can-parse?-fn can-parse?
    ::parser/parse-fn     ->node})
-
-(s/valid? ::parser/t parser)
 
 (s/fdef create-vault-file
   :args (s/cat :vault ::vault/t :links ::node/links)
@@ -38,9 +44,7 @@
 (defn create-vault-file
   [vault links]
   (let [vault-file (update (fixtures/vault-file) ::vault-file/id (fn [f] (File.  (str f extension))))
-        file       (#'vault/vault-file->java-file vault vault-file)]
-    (spit file
-          (->> links
-               (map str)
-               (str/join "\n")))
+        file       (#'vault/vault-file->java-file vault vault-file)
+        content    (->> links (map str) (str/join "\n"))]
+    (spit file content)
     vault-file))
