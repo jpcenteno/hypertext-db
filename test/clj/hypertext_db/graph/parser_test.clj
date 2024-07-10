0fdd0c6c3d2e1d2be60e1f4a3929015da528f105
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-07-09 22:12:47 -0300

    Refactor graph.parser-test

--- test/clj/hypertext_db/graph/parser_test.clj
+++ test/clj/hypertext_db/graph/parser_test.clj
@@ -10,31 +10,27 @@
   (:import (java.io File)))
 
 (deftest test-parse
-  (testing "Provided an empty `parser-chain`"
+  (testing "Provided an empty `::parser/parser-chain` and a random `::vault-file/t`"
     (is (let [vault      (fixtures/vault)
               vault-file (fixtures/vault-file)]
-          (testing "Returns the same value as `node/vault-file->`"
+          (testing "falls back to `node/vault-file->`"
             (is (= (node/vault-file-> vault-file)
                    (parser/parse [] vault-file vault)))))))
 
   (testing "Provided a parser in the chain"
     (is (let [vault           (fixtures/vault)
-              vault-file-cant (fixtures/vault-file)
               links           #{(File. "link-1.md") (File. "link-2.png")}
               vault-file-can  (simple-parser/create-vault-file vault links)
+              vault-file-cant (fixtures/vault-file)
               parsers         [simple-parser/parser]]
 
-          (testing "And a `vault-file` that the parser CAN'T parse"
-            (testing "it returns the same value as `node/vault-file->`"
+          (testing "and a `::vault-file/t` that the parser CAN'T parse"
+            (testing "it falls back to `node/vault-file->`"
               (is (= (node/vault-file-> vault-file-cant)
                      (parser/parse parsers vault-file-cant vault)))))
 
-          (testing "And a `vault-file` that the file parser CAN parse"
-            (is (let [result (parser/parse parsers vault-file-can vault)]
-
-                  (testing "it returns the same value as the parser's `::parser/parse-fn`"
-                    (is (= ((::parser/parse-fn simple-parser/parser) vault-file-can vault)
-                           result)))
-
-                  (testing "with all the links passed to the fixture"
-                    (is (= links (::node/links result)))))))))))
+          (testing "and a `::vault-file/t` that the file parser CAN parse"
+            (testing "it returns the same result as evaluating the parser's `::parser/parse-fn`"
+              (is (let [expected ((::parser/parse-fn simple-parser/parser) vault-file-can vault)
+                        result   (parser/parse parsers vault-file-can vault)]
+                    (is (= expected result))))))))))
