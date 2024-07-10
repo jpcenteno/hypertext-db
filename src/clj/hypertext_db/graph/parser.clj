dcda25e39293e9fe235dd5d516d04bc6710c62af
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-07-09 22:41:23 -0300

    Clean up the `parser` namespace

--- src/clj/hypertext_db/graph/parser.clj
+++ src/clj/hypertext_db/graph/parser.clj
@@ -1,38 +1,51 @@
 (ns hypertext-db.graph.parser
+  "This namespace defines a structure and helpers to declare parsers that read
+  files from the vault and convert them into graph nodes."
   (:require [clojure.spec.alpha            :as s]
             [hypertext-db.graph.node       :as node]
             [hypertext-db.vault            :as vault]
             [hypertext-db.vault.vault-file :as vault-file]))
 
-; (s/def ::can-parse?-fn (s/fspec :args (s/cat :vault-file ::vault-file/t) :ret boolean?))
-; (s/def ::parse-fn      (s/fspec :args (s/cat :vault-file ::vault-file/t :vault ::vault/t) :ret ::node/t))
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Type specs                                                             ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
 (s/def ::can-parse?-fn ifn?)
 (s/def ::parse-fn      ifn?)
 (s/def ::t             (s/keys :req [::can-parse?-fn ::parse-fn]))
 
 (s/def ::parser-chain  (s/coll-of ::t :kind sequential?))
 
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Implementation                                                         ║
+; ╚════════════════════════════════════════════════════════════════════════╝
+
 (defn- default-parse-fn
   [vault-file _vault]
   (node/vault-file-> vault-file))
 
-(s/fdef get-parser-fn*
+(s/fdef select-parser-fn*
   :args (s/cat :parser-chain ::parser-chain :vault-file ::vault-file/t)
   :ret (s/nilable ::parse-fn))
-(defn- get-parser-fn*
+(defn- select-parser-fn*
   [parser-chain vault-file]
   (::parse-fn (first (filter #((::can-parse?-fn %) vault-file) parser-chain))))
 
-(s/fdef get-parser-fn
+(s/fdef select-parse-fn
   :args (s/cat :parser-chain ::parser-chain :vault-file ::vault-file/t)
   :ret ::parse-fn)
-(defn- get-parse-fn
+(defn- select-parse-fn
   [parser-chain vault-file]
-  (or (get-parser-fn* parser-chain vault-file) default-parse-fn))
+  (or (select-parser-fn* parser-chain vault-file) default-parse-fn))
+
+; ╔════════════════════════════════════════════════════════════════════════╗
+; ║ Public API                                                             ║
+; ╚════════════════════════════════════════════════════════════════════════╝
 
 (s/fdef parse
   :args (s/cat :parser-chain ::parser-chain :vault-file ::vault-file/t :vault ::vault/t)
   :ret  ::node/t)
 (defn parse
+  "Parses a `vault-file` selecting the first parser capable of handling it."
   [parser-chain vault-file vault]
-  ((get-parse-fn parser-chain vault-file) vault-file vault))
+  ((select-parse-fn parser-chain vault-file) vault-file vault))
